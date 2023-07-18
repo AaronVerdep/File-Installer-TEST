9acbe8468ddcc9760ce45017188483d209c96914
AaronVerdep <139834084+AaronVerdep@users.noreply.github.com>
2023-07-17 23:37:12 -0400

    Add files via upload

---
+++ Install/My File.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Welcome to My File of GitHub!
+echo You will install a File in your pc!
+echo Press any key if youre ready!
+pause
+echo Ok, Downloading File...
+echo File downloaded!
+echo WARNING: The file its just for windows!
+md My File
